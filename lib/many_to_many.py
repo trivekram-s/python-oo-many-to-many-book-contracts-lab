7b19925610c2e3262200faab994c9158d0acaabb
trivekram-s <s.trivekram@genpact.com>
2026-06-21 12:30:22 +0530

    Initial commit

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,58 @@
-class Author:
-    pass
-
 
 class Book:
-    pass
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [c for c in Contract.all if c.book == self]
+
+    def authors(self):
+        return [c.author for c in self.contracts()]
+
+
+class Author:
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [c for c in Contract.all if c.author == self]
+
+    def books(self):
+        return [c.book for c in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+
+    def total_royalties(self):
+        return sum(c.royalties for c in self.contracts())
 
 
 class Contract:
-    pass
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        if not isinstance(author, Author):
+            raise Exception()
+        if not isinstance(book, Book):
+            raise Exception()
+        if not isinstance(date, str):
+            raise Exception()
+        if not isinstance(royalties, int):
+            raise Exception()
+
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties
+
+        Contract.all.append(self)
+
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [c for c in cls.all if c.date == date]
